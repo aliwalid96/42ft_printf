cbabc5c1bd3b621c9e27e092484a5d0c58c08b6b
Ali Muhsen <amuhsen@c1r6s6.42amman.com>
2024-10-11 23:38:05 +0300

    add repo link

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # 42ft_printf
+
+https://github.com/artix31/Libftprintf-ft_printf-/blob/main/ft_printf.c
+
---
+++ ft_printf.c
@@ -0,0 +1,19 @@
+#include <stdarg.h>
+#include <stdio.h>
+// int ft_printf(const char *item, ...)
+// {
+// // va_list arg;
+// // int size;
+
+// // size = 0;
+// // va_start (arg, item);
+
+
+// // va_end (arg);
+// // return (size);
+// }
+
+int main()
+{
+    printf("", 5);
+}
